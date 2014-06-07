bd7d7d1a1bc0741d5eda0afc0763d13143a9ff2f
gatis <gatis.riders@gmail.com>
2014-06-07 19:36:41 +0300

    testing simple android widget

---
+++ testWidget/src/com/example/testwidget/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.testwidget;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RemoteViews;
+import android.os.Build;
+
+public class MainActivity extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		for (int i = 0; i < appWidgetIds.length; i++) {
+			int appWidgetId = appWidgetIds[i];
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://reach.id.lv"));
+			PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_main);
+			views.setOnClickPendingIntent(R.id.imageButton1, pending);
+			appWidgetManager.updateAppWidget(appWidgetId, views);
+		}
+	}
+
+
+}
